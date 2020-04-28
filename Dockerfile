97ef3fa4fb57aa6f673dd498f68ce771994cd149
Daniel Bryant <daniel.bryant@tai-dev.co.uk>
2020-04-28 14:28:16 +0100

    Add new testing route

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,6 @@ COPY pom.xml .
 RUN mvn clean package
 
 FROM openjdk:8-jre
-COPY --from=build target/shopfront-0.0.1-SNAPSHOT.jar app.jar
+COPY --from=BUILD target/shopfront-0.0.1-SNAPSHOT.jar app.jar
 EXPOSE 8010
 ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/app.jar"]
---
+++ src/main/java/uk/co/danielbryant/djshopping/shopfront/controllers/JsonController.java
@@ -0,0 +1,14 @@
+package uk.co.danielbryant.djshopping.shopfront.controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JsonController {
+
+    @RequestMapping("/test")
+    public String test() {
+        return "{\"result\":\"okay\"}";
+    }
+}
